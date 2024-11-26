606ac531b14d2c25716ab5dd15aaef66498125b2
EricS02 <124088002+EricS02@users.noreply.github.com>
2024-11-25 21:02:58 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import "./App.css";
+import Tasks from "./components/Tasks";
+import { useState, useEffect } from "react";
+import AddTask from "./components/AddTask";
 
-function App() {
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+      getTasks()
+    }, [])
+
+    //Fetch tasks
+    const fetchTasks = async () => {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+
+      return data
+    }
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000 + 1);
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+  //Delete task
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE'
+    })
+
+
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+  //toggleReminder
+  const toggleReminder = (id) => {
+    console.log("Toggling reminder for task:", id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+    console.log("Updated tasks:", tasks);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header
+        title="Task Tracker"
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No info to show"
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Task.js
@@ -0,0 +1,23 @@
+import { FaTimes } from "react-icons/fa";
+const Task = ({ task, onDelete, onToggle }) => {
+  return (
+    <div
+      className={`task ${task.reminder ? "reminder" : ""}`}
+      onDoubleClick={() => {
+        console.log("Double click detected on task:", task.id);
+        onToggle(task.id);
+      }}
+    >
+      <h3>
+        {task.title}{" "}
+        <FaTimes
+          style={{ color: "red", cursor: "pointer" }}
+          onClick={() => onDelete(task.id)}
+        />
+      </h3>
+      <p>{task.day}</p>
+    </div>
+  );
+};
+
+export default Task;
